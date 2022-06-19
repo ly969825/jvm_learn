6ff3dacae15fa0525e88f7533018830d04183b5d
luyue0220 <284425540@qq.com>
2022-06-19 19:10:19 +0800

    第七周作业

--- jvm_learn/src/three_week/NettyHttp.java
+++ jvm_learn/src/three_week/NettyHttp.java
@@ -2,13 +2,12 @@ package three_week;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
